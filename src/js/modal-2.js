175f5a1626e586700c6df64c34fcd8e677835517
Yuriy Pilipenko <yurik2061@gmail.com>
2022-12-28 15:37:59 +0200

    script

---
+++ src/js/modal-2.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($){
+    //open popup
+    $('.popup-trigger').on('click', function(event){
+      event.preventDefault();
+      var number = $(this).data("number");
+      $('.popup.popup-'+number).addClass('is-visible');
+    });
+    
+    //close popup
+    $('.popup').on('click', function(event){
+      if( $(event.target).is('.popup-close') || $(event.target).is('.popup') ) {
+        event.preventDefault();
+        $(this).removeClass('is-visible');
+      }
+    });
+    
+    //close popup when clicking the esc keyboard button
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+          $('.popup').removeClass('is-visible');
+        }
+      });
+  });
