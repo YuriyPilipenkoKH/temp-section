ac658c586e4df26d410bc95bddb09fb53237c64d
Yuriy Pilipenko <yurik2061@gmail.com>
2022-12-29 22:02:35 +0200

    snow

---
+++ src/js/snow-3d.js
@@ -0,0 +1,16 @@
+https://cdn.jsdelivr.net/npm/christmas-snow-3d/build/snow3d.js
+
+// <script async src="https://cdn.jsdelivr.net/npm/christmas-snow-3d/build/snow3d.js"></script>
+
+function startjs() {
+
+    document.head.appendChild((function() {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'https://cdn.jsdelivr.net/npm/christmas-snow-3d/build/snow3d.js';
+        return s;
+    })());
+
+}
+
